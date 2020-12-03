4478523a4154a7704e5ce3a9d498841b608a1d81
Benno Rice <benno@jeamland.net>
2020-12-03 18:47:35 +1100

    Day 3, part 2.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,6 +4,37 @@ use std::io::BufReader;
 
 use clap::{App, Arg};
 
+const SLOPES: &[(usize, usize)] = &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+struct TreeField(Vec<Vec<bool>>);
+
+impl TreeField {
+    fn new(data: Vec<Vec<bool>>) -> Self {
+        Self(data)
+    }
+
+    fn count_trees(&self, right: usize, down: usize) -> usize {
+        let row_len = self.0[0].len();
+
+        let mut row_pos = 0;
+        let mut tree_count = 0;
+
+        for (i, row) in self.0.iter().enumerate() {
+            if i % down != 0 {
+                continue;
+            }
+
+            if row[row_pos] {
+                tree_count += 1;
+            }
+
+            row_pos = (row_pos + right) % row_len;
+        }
+
+        tree_count
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let matches = App::new("AOC2020 Day 3")
         .arg(
@@ -18,27 +49,23 @@ fn main() -> std::io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut field: Vec<Vec<bool>> = Vec::new();
-    let mut row_len = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
 
         let row: Vec<bool> = line.chars().map(|c| c == '#').collect();
-        row_len = row.len();
         field.push(row);
     }
 
-    let mut row_pos = 0;
-    let mut tree_count = 0;
-    for row in field.iter() {
-        if row[row_pos] {
-            tree_count += 1;
-        }
+    let field = TreeField::new(field);
+    let mut product = 1;
 
-        row_pos = (row_pos + 3) % row_len;
+    for (right, down) in SLOPES.iter() {
+        let tree_count = field.count_trees(*right, *down);
+        println!("Right {}, down {}: {} trees", right, down, tree_count);
+        product *= tree_count;
     }
-
-    println!("{} trees", tree_count);
+    println!("Final product: {}", product);
 
     Ok(())
 }
