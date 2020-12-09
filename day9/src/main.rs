372718197f0d5bbe1c0724c0cb4fae5b07441cd0
Benno Rice <benno@jeamland.net>
2020-12-09 16:54:15 +1100

    Day 9, part 1.

---
+++ day9/src/main.rs
@@ -0,0 +1,81 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::str::FromStr;
+
+use clap::{App, Arg};
+use itertools::Itertools;
+
+struct NumberBuffer {
+    buffer: Vec<usize>,
+    preamble: usize,
+}
+
+impl NumberBuffer {
+    fn new(preamble: usize) -> Self {
+        Self {
+            buffer: Vec::new(),
+            preamble,
+        }
+    }
+
+    fn push(&mut self, number: usize) {
+        self.buffer.push(number);
+        if self.buffer.len() > self.preamble {
+            self.buffer.remove(0);
+        }
+    }
+
+    fn ready(&self) -> bool {
+        self.buffer.len() >= self.preamble
+    }
+
+    fn is_sum_of_preamble_pair(&self, number: usize) -> bool {
+        for pair in self.buffer.iter().combinations(2) {
+            if number == pair.iter().copied().sum() {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let matches = App::new("AOC2020 Day 4")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input file name")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("PREAMBLE")
+                .short("p")
+                .long("preamble")
+                .help("Preamble length")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let file = File::open(matches.value_of("INPUT").unwrap())?;
+    let reader = BufReader::new(file);
+
+    let preamble = usize::from_str(matches.value_of("PREAMBLE").unwrap()).unwrap();
+    let mut buffer = NumberBuffer::new(preamble);
+
+    for number in reader
+        .lines()
+        .map(|l| usize::from_str(l.unwrap().as_str()).unwrap())
+    {
+        if buffer.ready() && !buffer.is_sum_of_preamble_pair(number) {
+            println!("{}", number);
+            break;
+        }
+
+        buffer.push(number);
+    }
+
+    Ok(())
+}
