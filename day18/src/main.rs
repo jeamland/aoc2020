ca71557b36a578f3592b1f981b9d647ee27a4e6b
Benno Rice <benno@jeamland.net>
2020-12-19 22:47:45 +1100

    Day 18, part 2.

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -12,7 +12,7 @@ enum Operator {
     Multiply,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Token {
     Add,
     Multiply,
@@ -78,6 +78,74 @@ fn evaluate<I: Iterator<Item = Token>>(tokens: &mut I) -> usize {
     accumulator
 }
 
+fn evaluate2(tokens: Vec<Token>) -> usize {
+    let mut tokens = tokens;
+    let mut new_tokens = Vec::new();
+
+    while let Some(start) = tokens.iter().position(|t| *t == Token::StartParen) {
+        let mut end = start;
+        let mut paren_depth = 0;
+        for (pos, token) in tokens.iter().enumerate().skip(start + 1) {
+            match token {
+                Token::StartParen => paren_depth += 1,
+                Token::EndParen => {
+                    if paren_depth == 0 {
+                        end = pos;
+                        break;
+                    } else {
+                        paren_depth -= 1;
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        let paren_span: Vec<Token> = tokens
+            .iter()
+            .skip(start + 1)
+            .take(end - start - 1)
+            .copied()
+            .collect();
+
+        let result = evaluate2(paren_span);
+
+        new_tokens.extend(tokens.iter().take(start).copied());
+        new_tokens.push(Token::Value(result));
+        new_tokens.extend(tokens.iter().skip(end + 1).copied());
+        tokens = new_tokens;
+        new_tokens = Vec::new();
+    }
+
+    let mut in_add = false;
+    let mut lhs = 0;
+
+    for token in tokens.iter() {
+        match token {
+            Token::Value(value) => {
+                if in_add {
+                    lhs += value;
+                    new_tokens.pop();
+                    new_tokens.push(Token::Value(lhs));
+                    in_add = false;
+                } else {
+                    lhs = *value;
+                    new_tokens.push(Token::Value(*value));
+                }
+            }
+            Token::Add => in_add = true,
+            t => new_tokens.push(*t),
+        }
+    }
+
+    new_tokens
+        .iter()
+        .filter_map(|t| match t {
+            Token::Value(x) => Some(x),
+            _ => None,
+        })
+        .product()
+}
+
 fn main() -> std::io::Result<()> {
     let matches = App::new("AOC2020 Day 18")
         .arg(
@@ -91,19 +159,28 @@ fn main() -> std::io::Result<()> {
     let file = File::open(matches.value_of("INPUT").unwrap())?;
     let reader = BufReader::new(file);
 
-    let mut sum = 0;
+    let mut sum1 = 0;
+    let mut sum2 = 0;
+
     for line in reader.lines() {
         let line = line.unwrap();
 
         let parts: Vec<String> = line.split(' ').map(|v| v.to_string()).collect();
-        let result = evaluate(&mut tokenise(parts).iter().cloned());
-        sum += result;
+        let tokens = tokenise(parts);
 
-        println!("{} = {}", line, result);
+        let result1 = evaluate(&mut tokens.iter().cloned());
+        sum1 += result1;
+
+        let result2 = evaluate2(tokens);
+        sum2 += result2;
+
+        println!("{} = {}", line, result1);
+        println!("{} = {}", line, result2);
+        println!();
     }
 
-    println!();
-    println!("Total: {}", sum);
+    println!("Total 1: {}", sum1);
+    println!("Total 2: {}", sum2);
 
     Ok(())
 }
